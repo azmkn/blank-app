1249c3aa6a3a65bf3cd2885ceae742c58afab720
azmkn <dog.galaxy.2020@gmail.com>
2026-03-23 02:14:12 +0000

    werewolf

---
+++ R(n).py
@@ -0,0 +1,20 @@
+import streamlit as st
+n=st.number_input('数',1)
+or_n=n
+exp=0
+b=1
+while n%2==0:
+  n//=2
+while n%5==0:
+  n//=5
+if n==1:
+  st.text(str(or_n)+":"+str(exp))
+while True:
+  b*=10
+  exp+=1
+  b%=n
+  if b==1:
+
+    if st.button("表示"):
+      st.text(str(or_n)+"   :   "+str(exp))
+      exit()
