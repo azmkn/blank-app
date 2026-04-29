79b5151863b43a59f092d052f0bdaa0974f1737a
azmkn <dog.galaxy.2020@gmail.com>
2026-04-29 13:38:28 +0000

    おい

--- progression.py
+++ progression.py
@@ -1,52 +1,106 @@
 import streamlit as st
 import pandas as pd
+import io
 
 excel_file=st.file_uploader("Excelファイルを選択してください",type=['xlsx'])
 
 if excel_file:
-    if "df" not in st.session_state:
-        st.session_state.df=pd.read_excel(excel_file)
-    problem_number_list=st.session_state.df['問題番号']
-    problem_number=st.number_input('問題番号を選択してください',problem_number_list)
-    problem_data=st.session_state.df.iloc[problem_number]
-    now_count=0
-    for i in range(3):
-        if problem_data[str(i+1)+'回目']=='-':
-            now_count=i
-            break
-    else:
-        st.text('回数が3回を超過しています')
-    if "choice" not in st.session_state:
-        st.session_state.choice = None
+    st.session_state.df=pd.read_excel(excel_file)
 
-    col1, col2, col3 = st.columns(3)
+    if "changes" not in st.session_state:
+        st.session_state.changes=[]
+
+    if "choice" not in st.session_state:
+        st.session_state.choice=None
+
+    if "confirm_clear" not in st.session_state:
+        st.session_state.confirm_clear=False
+
+    problem_number=st.number_input(
+        '問題番号を選択してください',
+        1,
+        int(st.session_state.df['問題番号'].max())
+    )
+
+    st.write("判定を選択")
+
+    # ⭐ 横並び＆大きく
+    col1,col2,col3=st.columns(3)
 
     with col1:
-        if st.button("o"):
-            st.session_state.choice = "o"
+        if st.button("o",use_container_width=True):
+            st.session_state.choice="o"
 
     with col2:
-        if st.button("△"):
-            st.session_state.choice = "△"
+        if st.button("△",use_container_width=True):
+            st.session_state.choice="△"
 
     with col3:
-        if st.button("x"):
-            st.session_state.choice = "x"
-    st.text(st.session_state.choice)
+        if st.button("x",use_container_width=True):
+            st.session_state.choice="x"
 
-    if st.session_state.choice:
-        edited_df=st.data_editor(st.session_state.df)
-        edited_df.at[problem_number,now_count]=st.session_state.choice
-    st.write(edited_df)
+    st.write("現在の選択:",st.session_state.choice)
 
-    import io
+    if st.button("追加"):
+        if st.session_state.choice:
+            st.session_state.changes=[
+                c for c in st.session_state.changes
+                if c["problem"]!=problem_number
+            ]
+            st.session_state.changes.append({
+                "problem":problem_number,
+                "choice":st.session_state.choice
+            })
+
+    st.write("変更予定")
+
+    if st.session_state.changes:
+        changes_df=pd.DataFrame(st.session_state.changes)
+        changes_df.columns=["問題番号","判定"]
+        st.dataframe(changes_df)
+    else:
+        st.write("（まだ追加されていません）")
+
+    # ⭐ クリア確認フロー
+    if st.button("変更予定をクリア"):
+        st.session_state.confirm_clear=True
+
+    if st.session_state.confirm_clear:
+        st.warning("本当にクリアしますか？")
+        col1,col2=st.columns(2)
+
+        with col1:
+            if st.button("はい"):
+                st.session_state.changes=[]
+                st.session_state.confirm_clear=False
+
+        with col2:
+            if st.button("いいえ"):
+                st.session_state.confirm_clear=False
+
+    if st.button("変更"):
+        for change in st.session_state.changes:
+            p=change["problem"]
+            c=change["choice"]
+
+            problem_data=st.session_state.df.iloc[p-1]
+
+            for i in range(3):
+                col_name=str(i+1)+'回目'
+                if problem_data[col_name]=='-':
+                    st.session_state.df.at[p-1,col_name]=c
+                    break
+
+        st.session_state.changes=[]
+
+    st.dataframe(st.session_state.df)
 
     buffer=io.BytesIO()
     with pd.ExcelWriter(buffer,engine="openpyxl") as writer:
-        edited_df.to_excel(writer,index=False)
+        st.session_state.df.to_excel(writer,index=False)
 
     st.download_button(
         "ダウンロード",
         buffer.getvalue(),
-        "edited.xlsx"
+        excel_file.name
     )
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,55 +1,106 @@
 import streamlit as st
 import pandas as pd
+import io
 
 excel_file=st.file_uploader("Excelファイルを選択してください",type=['xlsx'])
-if excel_file:
-  df=pd.read_excel(excel_file)
-  class_list=df['学級']
-  your_class=st.selectbox('クラスを選択してください',class_list)
-  class_dict={}
-  for i in range(len(class_list)):
-    class_dict[class_list[i]]=i
-  from random import choice
 
-  if st.button("Start!"):
-    class_number=class_dict[your_class]
-    class_data=df.iloc[class_number]
-    n=class_data['人数']
-    attendance_number_start=class_data['最初の出席番号']
-    h,w=class_data['縦の長さ'],class_data['横の長さ']
-    g=class_data['席'].split()
-    grid=[]
-    for i in g:
-      grid.append(list(i))
-    l = [i for i in range(1, n+1)]
-    name = class_data['名前(出席番号順)'].split()
-    max_name_length=max([len(i)for i in name])
-    for i in range(len(name)):
-      if i+attendance_number_start<=9:p=str(i+attendance_number_start)+" "
-      else:p=str(i+attendance_number_start)
-      name[i]=p+"."+name[i]+"  "*(max_name_length-len(name[i]))
-    fix_number=class_data['固定人数']
-    if fix_number!=0:
-      fix_data=list(map(int,class_data['固定者の番号、位置'].split()))
-    for i in range(fix_number):
-      grid[fix_data[3*i+1]-1][fix_data[3*i+2]-1]=name[fix_data[3*i]-attendance_number_start]
-      l.remove(fix_data[3*i]-attendance_number_start+1)
-    tf=True
-    for i in range(0,h):
-      for j in range(0,w):
-        if grid[i][j]=="x":
-          grid[i][j]="   "+"  "*max_name_length
-          continue
-        if grid[i][j]=="o":
-          c=choice(l)
-          grid[i][j]=name[c - 1]
-          l.remove(c)
-    st.text("")
-    data={}
-    for i in range(w):
-      newl=[]
-      for j in range(h):
-        newl.append(grid[j][i])
-      data[i]=newl
-    df_seat=pd.DataFrame(data)
-    st.markdown(df_seat.to_html(index=False, header=False), unsafe_allow_html=True)
+if excel_file:
+    st.session_state.df=pd.read_excel(excel_file)
+
+    if "changes" not in st.session_state:
+        st.session_state.changes=[]
+
+    if "choice" not in st.session_state:
+        st.session_state.choice=None
+
+    if "confirm_clear" not in st.session_state:
+        st.session_state.confirm_clear=False
+
+    problem_number=st.number_input(
+        '問題番号を選択してください',
+        1,
+        int(st.session_state.df['問題番号'].max())
+    )
+
+    st.write("判定を選択")
+
+    # ⭐ 横並び＆大きく
+    col1,col2,col3=st.columns(3)
+
+    with col1:
+        if st.button("o",use_container_width=True):
+            st.session_state.choice="o"
+
+    with col2:
+        if st.button("△",use_container_width=True):
+            st.session_state.choice="△"
+
+    with col3:
+        if st.button("x",use_container_width=True):
+            st.session_state.choice="x"
+
+    st.write("現在の選択:",st.session_state.choice)
+
+    if st.button("追加"):
+        if st.session_state.choice:
+            st.session_state.changes=[
+                c for c in st.session_state.changes
+                if c["problem"]!=problem_number
+            ]
+            st.session_state.changes.append({
+                "problem":problem_number,
+                "choice":st.session_state.choice
+            })
+
+    st.write("変更予定")
+
+    if st.session_state.changes:
+        changes_df=pd.DataFrame(st.session_state.changes)
+        changes_df.columns=["問題番号","判定"]
+        st.dataframe(changes_df)
+    else:
+        st.write("（まだ追加されていません）")
+
+    # ⭐ クリア確認フロー
+    if st.button("変更予定をクリア"):
+        st.session_state.confirm_clear=True
+
+    if st.session_state.confirm_clear:
+        st.warning("本当にクリアしますか？")
+        col1,col2=st.columns(2)
+
+        with col1:
+            if st.button("はい"):
+                st.session_state.changes=[]
+                st.session_state.confirm_clear=False
+
+        with col2:
+            if st.button("いいえ"):
+                st.session_state.confirm_clear=False
+
+    if st.button("変更"):
+        for change in st.session_state.changes:
+            p=change["problem"]
+            c=change["choice"]
+
+            problem_data=st.session_state.df.iloc[p-1]
+
+            for i in range(3):
+                col_name=str(i+1)+'回目'
+                if problem_data[col_name]=='-':
+                    st.session_state.df.at[p-1,col_name]=c
+                    break
+
+        st.session_state.changes=[]
+
+    st.dataframe(st.session_state.df)
+
+    buffer=io.BytesIO()
+    with pd.ExcelWriter(buffer,engine="openpyxl") as writer:
+        st.session_state.df.to_excel(writer,index=False)
+
+    st.download_button(
+        "ダウンロード",
+        buffer.getvalue(),
+        excel_file.name
+    )
